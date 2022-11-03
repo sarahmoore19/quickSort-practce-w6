ca17d747ed7d45cbf5231358ae6fba59a6c8d476
sarahmoore <sarahnmoore19@gmail.com>
2022-11-03 11:37:29 -0400

    initial commit

--- quicksort.js
+++ quicksort.js
@@ -1,20 +1,44 @@
 function quicksort(arr) {
 
-  // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  if (arr.length <= 1) return arr;
+  let pivot = arr[0];
 
-  // Pick the first value as the pivot
+  let left = []
+  let right = []
 
-  // Orient the pivot so that...
-      // every number smaller than the pivot is to the left
-      // every number larger (or equal) than the pivot is to the right
+  for(let i = 1; i < arr.length; i++) {
+  if (arr[i] <= pivot) left.push(arr[i])
+  else right.push(arr[i])
+  }
 
-  // Recursively sort the left
-  // Recursively sort the right
-
-  // Return the left, pivot and right in sorted order
+   return [...quicksort(left), pivot, ...quicksort(right)]
 
 }
 
+function quicksortMyWay(arr) {
 
+  if (arr.length <= 1) return arr;
+  let pivot = arr.pop()
+
+  let first;
+  for(let i = 0; i < arr.length; i++) {
+  if (arr[i] > pivot && first === undefined) first = i;
+  if (arr[i] <= pivot && first !== undefined) {
+    [arr[i], arr[first]] = [arr[first], arr[i]];
+    first++;
+  }
+  }
+
+  let count = 0;
+  arr.forEach((ele) => {
+    if (ele <= pivot) count++
+  });
+
+  let right = arr.slice(count);
+  arr.length-= (arr.length - count)
+
+
+   return [...quicksort(arr), pivot, ...quicksort(right)]
+
+}
 module.exports = [quicksort];
